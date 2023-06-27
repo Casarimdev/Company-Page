520ad7f1b1667f0bb9504c4cbf015730b63a52f9
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2023-06-27 15:21:33 -0300

    ...

--- scripts/clientes.js
+++ scripts/clientes.js
@@ -1,13 +1,12 @@
 "use strict";
 
 function carousel() {
-  let carouselSlider =                document.querySelector(".carousel__slider");
+  let carouselSlider = document.querySelector(".carousel__slider");
   let list = document.querySelector(".carousel__list");
   let item = document.querySelectorAll(".carousel__item");
   let list2;
 
   const speed = 0.6;
-
   const width = list.offsetWidth;
   let x = 0;
   let x2 = width;
@@ -18,35 +17,27 @@ function carousel() {
     list2.style.left = `${width}px`;
   }
 
-  function moveFirst() {
+  function move() {
     x -= speed;
-
-    if (width >= Math.abs(x)) {
-      list.style.left = `${x}px`;
-    } else {
-      x = width;
-    }
-  }
-
-  function moveSecond() {
     x2 -= speed;
 
-    if (list2.offsetWidth >= Math.abs(x2)) {
-      list2.style.left = `${x2}px`;
-    } else {
-      x2 = width;
+    if (x < -width) {
+      x += width;
     }
+
+    if (x2 < -width) {
+      x2 += width;
+    }
+
+    list.style.left = `${x}px`;
+    list2.style.left = `${x2}px`;
+
+    requestAnimationFrame(move);
   }
 
-  
-
- 
-
   clone();
+  requestAnimationFrame(move);
 
-  let a = setInterval(moveFirst, 10);
-  let b = setInterval(moveSecond, 10);
-  
   carouselSlider.addEventListener("mouseleave", unhover);
 }
 
