b365e012bd10224acae58e6633b67c506120267c
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2023-06-27 17:57:41 -0300

    ...

--- scripts/scriptmenu.js
+++ scripts/scriptmenu.js
@@ -1,5 +1,9 @@
-const menuBtn = document.querySelector(".menumobile-btn");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".menudesktop-btn");
+  const menuMobile = document.querySelector(".navbarmobile");
+  const menuMobileLinks = document.querySelector(".menumobile-links");
 
-menuBtn.addEventListener("click", () => {
-  document.body.classList.toggle("open");
+  menuBtn.addEventListener("click", function () {
+    menuMobileLinks.classList.toggle("active");
+  });
 });
