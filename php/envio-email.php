77998c618e15e18153c59911d8f16779971f2eab
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2023-10-17 16:36:16 -0300

    update.php

---
+++ php/envio-email.php
@@ -0,0 +1,55 @@
+<?php
+
+
+$nome = $_POST['Nome'];
+  $email = $_POST['Email'];
+  $number = $_POST['Celular'];
+  $empresa = $_POST['Empresa'];
+  $mensagem = $_POST['Mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+if (isset($_POST['enviar'])){
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'casarim.dev@gmail.com';                     //SMTP username
+        $mail->Password   = 'Porta369';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('casarim.dev@gmail.com', 'Mailer');
+        $mail->addAddress('casarim.dev@gmail.com', 'Joe User');     //Add a recipient
+        // $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('casarim.dev@gmail.com', 'Information');
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Contato pelo site';
+        $mail->Body    = '<p><b>Nome: </b>$nome</p>
+        <p><b>E-mail: </b>$email</p>
+        <p><b>Empresa: </b>$empresa</p>
+        <p><b>Numero: </b>$number</p>
+        <p><b>Mensagem: </b>$mensagem</p>
+        <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail-'E-mail enviado com sucesso!';
+    } catch (Exception $e) {
+        echo "Erro ao enviar o e-mail: {$mail->ErrorInfo}";
+    }
+}
