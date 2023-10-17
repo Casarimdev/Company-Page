4369d0486e5f3092cd62215256fd927ac652a143
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2023-10-17 13:07:21 -0300

    form

---
+++ scripts/submit.js
@@ -0,0 +1,35 @@
+let inputName = document.getElementById("inputName")
+let inputNumber = document.getElementById("inputNum")
+let inputEmpresa = document.getElementById("inputEmp")
+let inputEmail = document.getElementById("inputEmail")
+let areaMensagem = document.getElementById("textArea")
+let buttonSubmit = document.getElementById("submit")
+
+function Send(){
+    if (inputName.value == ""){
+        alert('Preencha o seu nome.')
+    }
+    
+    else if (inputEmail.value == ""){
+        alert('Preencha seu e-mail.')
+    }
+
+    else if (inputEmpresa.value == ""){
+        alert('Preencha o nome de sua empresa.')
+    }
+
+    else if (inputNumber.value == ""){
+        alert('Preencha seu número.')
+    }
+
+    else{
+        alert('E-mail enviado com sucesso! Entraremos em contato.')
+        inputEmail.value = ""
+        areaMensagem.value = ""
+        inputName.value = ""
+        inputEmpresa.value = ""
+        inputNumber.value = ""
+    }
+    
+    
+}
