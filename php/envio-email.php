6c954748e8b2a8130b78156c0962bacdf15e1dad
Lucas Casarim de Lima <116107342+Casarimdev@users.noreply.github.com>
2023-10-17 13:42:22 -0300

    p

---
+++ php/envio-email.php
@@ -0,0 +1,36 @@
+<?php
+  //Variáveis
+  $nome = $_POST['Nome'];
+  $email = $_POST['Email'];
+  $number = $_POST['Celular'];
+  $empresa = $_POST['Empresa'];
+  $mensagem = $_POST['Mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Empresa: </b>$empresa</p>
+      <p><b>Numero: </b>$number</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "casarim.dev@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+?>
